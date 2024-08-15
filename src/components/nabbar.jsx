5cf154eeecd4fa5d6bb76d3a948e19b83b1e504e
KhEliash <eliashebrahim@gmail.com>
2024-08-15 14:32:45 +0600

    add:logout and button shifting

--- src/components/nabbar.jsx
+++ src/components/nabbar.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 function Nabbar() {
+  const { user, logOut } = useContext(AuthContext);
   return (
     <div>
       <div className="navbar bg-base-200">
@@ -22,58 +25,27 @@ function Nabbar() {
                 />
               </svg>
             </div>
-            {/* <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul> */}
           </div>
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
-        {/* <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div> */}
+
         <div className="navbar-end">
-          <Link to="/login">
-            <button className="btn btn-primary">Login</button>
-          </Link>
+          {user ? (
+            <>
+              <button
+                className="btn btn-error text-white"
+                onClick={() => logOut()}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <button className="btn btn-primary">Login</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
