9507e1f4d19c0b6f841ed00ba8b394c36206504d
KhEliash <eliashebrahim@gmail.com>
2024-08-16 07:28:05 +0600

    add: paiginations buttons

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -5,22 +5,55 @@ import Card from "../components/Card";
 function Home() {
   const [products, setProducts] = useState([]);
   console.log(products);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   useEffect(() => {
     axios
-      .get("http://localhost:5000/product")
-      .then((res) => setProducts(res.data));
-  }, []);
+      .get(`http://localhost:5000/product?page=${currentPage}&limit=10`)
+      .then((res) => {
+        // setProducts(res.data)
+        setProducts(res.data.products), setTotalPages(res.data.totalPages);
+      });
+  }, [currentPage]);
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
     <>
-   <div className="grid gap-5 px-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
-   {
-        products.map((p)=><Card 
-        key={p._id}
-        product={p}
-        >hi
-        </Card>)
-    }
-   </div>
+      <div className="grid gap-5 px-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+        {products.map((p) => (
+          <Card key={p._id} product={p}>
+            hi
+          </Card>
+        ))}
+      </div>
+      <div className="pagination text-center my-3 space-x-2">
+        <button
+          className="btn btn-primary"
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span className="font-bold">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          className="btn btn-primary"
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </>
   );
 }
